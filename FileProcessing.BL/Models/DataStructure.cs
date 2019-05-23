6f3f4ce0ed863d618709b0ec4e0882e066e53635
Михаил Масный <masny.mikhail@gmail.com>
2019-05-23 23:00:05 +0300

    Implemented basic application model - DataStructure.

---
+++ FileProcessing.BL/Models/DataStructure.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileProcessing.BL.Models
+{
+    /// <summary>
+    /// Структура данных.
+    /// </summary>
+    class DataStructure
+    {
+        /// <summary>
+        /// Режим получения данных.
+        /// </summary>
+        public string Input_mode { get; set; }
+
+        /// <summary>
+        /// Адрес получения данных.
+        /// </summary>
+        public string Input_address { get; set; }
+
+        /// <summary>
+        /// Список входных данных.
+        /// </summary>
+        public List<string> ListInputData = new List<string>();
+
+        /// <summary>
+        /// Пустой конструктор.
+        /// </summary>
+        public DataStructure() { }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="input_mode">режим.</param>
+        /// <param name="input_address">адрес.</param>
+        public DataStructure(string input_mode, string input_address)
+        {
+            if (string.IsNullOrWhiteSpace(input_mode) || string.IsNullOrWhiteSpace(input_address))
+            {
+                throw new ArgumentNullException("Accepted arguments cannot be empty or null.");
+            }
+
+            Input_mode = input_mode;
+            Input_address = input_address;
+        }
+    }
+}
