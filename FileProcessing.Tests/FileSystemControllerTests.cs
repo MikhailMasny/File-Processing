e22fbb0f91e741505b7716b11951b04e9954ffa2
Михаил Масный <masny.mikhail@gmail.com>
2019-08-17 20:24:00 +0300

    Implemented tests for the main methods.

---
+++ FileProcessing.Tests/FileSystemControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileProcessing.BL.Controllers.Implementations;
+using System;
+using System.Collections.Generic;
+using FileProcessing.BL.Models;
+
+namespace FileProcessing.BL.Tests
+{
+    [TestClass()]
+    public class FileSystemControllerTests
+    {
+        [TestMethod()]
+        public void CheckForSpecifiedPathTest()
+        {
+            // Arrange
+            var dataStructure = new DataStructure
+            {
+                Input_address = Guid.NewGuid().ToString()
+            };
+            var fileSystemController = new FileSystemController(dataStructure);
+
+            // Act
+            var result = fileSystemController.CheckForSpecifiedPath();
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void GetListOfFilesTest()
+        {
+            // Arrange
+            var dataStructure = new DataStructure
+            {
+                Input_address = Guid.NewGuid().ToString()
+            };
+            var fileSystemController = new FileSystemController(dataStructure);
+
+            // Act
+            var result = fileSystemController.GetListOfFiles();
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void ProcessInputDataTest_Return_True()
+        {
+            // Arrange
+            var listOfInputData = new List<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                listOfInputData.Add($"{Guid.NewGuid().ToString()},{i}");
+            }
+
+            var dataStructure = new DataStructure
+            {
+                ListOfInputData = listOfInputData
+            };
+            var fileSystemController = new FileSystemController(dataStructure);
+
+            // Act
+            var result = fileSystemController.ProcessInputData();
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void WriteDataToFileTest_Return_True()
+        {
+            // Arrange
+            var listOfProcessedData = new Dictionary<string, int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                listOfProcessedData.Add(Guid.NewGuid().ToString(), i);
+            }
+
+            var dataStructure = new DataStructure
+            {
+                ListOfProcessedData = listOfProcessedData
+            };
+            var fileSystemController = new FileSystemController(dataStructure);
+
+            // Act
+            var result = fileSystemController.WriteDataToFile();
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ FileProcessing.Tests/HttpControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileProcessing.BL.Controllers.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileProcessing.BL.Tests
+{
+    [TestClass()]
+    public class HttpControllerTests
+    {
+        [TestMethod()]
+        public void StartProcessingTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
